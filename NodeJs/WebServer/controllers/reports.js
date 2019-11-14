0dd17263967635d24a70977a0b9f8db0443da767
vikas <vikas@demo.com>
2019-11-14 15:30:13 +0530

    Basic WebServer - Update

---
+++ NodeJs/WebServer/controllers/reports.js
@@ -0,0 +1,31 @@
+const fs = require("fs").promises;
+
+const path = require("path");
+
+async function reportsController(req, res)
+{
+    try
+    {
+        res.writeHead(
+            200, // HTTP Status
+            {
+                "Content-Type": "text/html",
+                "ServedBy": "Custom NodeJS Server",
+            } // HTTP response headers
+        );
+
+        var data = await fs.readFile(
+            path.join(__dirname, "../views", "reports.html"),
+            "UTF-8"
+        );
+
+        res.end(data);
+    }
+    catch (ex)
+    {
+        console.log(ex);
+    }
+
+}
+
+module.exports = reportsController;
--- NodeJs/WebServer/server.js
+++ NodeJs/WebServer/server.js
@@ -1,17 +1,39 @@
 const http = require("http");
 
+const fs = require("fs").promises;
+const path = require("path");
+const homeController = require("./controllers/home");
+const reportsController = require("./controllers/reports");
+
+
 var httpServer = http.createServer(
     function httpHandler(request, response, next) // called for all incoming requests
     {
-        response.writeHead(
-            200, // HTTP Status
-            {
-                "Content-Type": "text/html",
-                "ServedBy": "Custom NodeJS Server",
-            } // HTTP response headers
-        );
+        try
+        {
+            var url = request.url;
+            console.log(url);
 
-        response.end("<h1>Served by NodeJS on " + new Date().toLocaleTimeString());
+            if (url == "/" || url == "/home")
+            {
+                homeController(request, response);
+            }
+            else if (url == "/reports")
+            {
+                reportsController(request, response);
+            }
+            else // 404
+            {
+                response.writeHead(404, {});
+            }
+        }
+        catch (ex)
+        {
+            console.log(ex);
+        }
+
+
+        //response.end("<h1>Served by NodeJS on " + new Date().toLocaleTimeString());
     }
 )
 
@@ -22,3 +44,8 @@ httpServer.listen(
         console.log("Server is up and running");
     }
 );
+
+
+
+
+
